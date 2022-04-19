0ec831517591ccc02917a9c7b5a1f1a3f734e47e
root <root@867813-cx28645.tmweb.ru>
2022-04-19 23:37:40 +0300

    new results

--- start.sh
+++ start.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-OUTPUT_PATH="$1"
 PROJECT_DIR="$(pwd)"
 
-if [[ ! -d venv ]]; then
-	python3 -m virtualenv venv
-fi
-
 source ./venv/bin/activate
-python3 -m pip install -U pip
-python3 -m pip install -r requirements.txt
-
-if [[ -z "$OUTPUT_PATH" ]]; then
-	python3 "$PROJECT_DIR/src/core/mig_synthesis.py"
-else
-	python3 "$PROJECT_DIR/src/core/mig_synthesis.py" -o "$OUTPUT_PATH"
-fi
+python3 "$PROJECT_DIR/src/core/mig_synthesis.py" $@
